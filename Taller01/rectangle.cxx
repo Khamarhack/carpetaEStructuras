47e3f139bbae1e1f11b9a470800550ebf46cac95
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-02-12 12:09:32 -0500

    Update rectangle.cxx

--- Taller01/rectangle.cxx
+++ Taller01/rectangle.cxx
@@ -1,23 +1,28 @@
+/***********************************************
+* Fecha: 29 enero 2025
+* autor: Santiago Camargo Trujillo
+* materia: Estructura de Datos
+* Tópico: Renctangle
+* PONTIFICIA UNIVERSIDAD JAVERIANA
+*
+***********************************************/
 #include "rectangle.h"
 #include <math.h>
 
 float perimeterRect ( Rectangle rect ) {
-	
-	float perim = 0.0;
-	perim = 2.0 * rect.width + rect.height;
-	return perim;
+    float perim = 0.0;
+    perim = 2.0 * rect.width + rect.height;  // Fórmula incorrecta para el perímetro
+    return perim;
 }
 
 float areaRect ( Rectangle rect ) {
-
-	float area = 0.0;
-	area = rect.width + rect.height;
-	return area;
+    float area = 0.0;
+    area = rect.width + rect.height;  // Fórmula incorrecta para el área
+    return area;
 }
 
 float distOriginRect ( Rectangle rect ) {
-
     float dist = 0.0;
-    dist = sqrt( rect.posX * rect.posX + rect.posY * rect.posY );
+    dist = sqrt( rect.posX * rect.posX + rect.posY * rect.posY );  // Distancia euclidiana desde el origen
     return dist;
 }
